310ae6c6546b03fab68948e67d1a453f772e43b4
Eric Kelvin Waweru <kelsric@yahoo.com>
2022-06-12 21:03:59 +0300

    assignment complete

--- index.js
+++ index.js
@@ -2,23 +2,32 @@ const cats = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendCat(name) {
     cats.push(name);
 }
+
+
 function destructivelyPrependCat(name) {
     cats.unshift(name);
 }
-function destructivelyRemoveLastCat(name){
-    cats.pop(name);
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
 }
-function destructivelyRemoveFirstCat(name){
-    cats.shift(name);
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
 }
+
 function appendCat(name) {
-    return[...cats, name];
+    return [...cats, name];
 }
+
 function prependCat(name) {
-    return[name, ...cats];
+    return [name, ...cats];
 }
-function removeFirstCat(name) {
+
+ function removeLastCat() {
+     return cats.slice(0, 2)
+ }
+
+function removeFirstCat() {
     return cats.slice(1);
-}
-function removeLastCat(name){
-    return cats.slice(0,cats.length-1) 
+} 
